07d000b47f8f45031bbef064dcf7cde6981a7486
Lucas Vieira <vieira07lucas@gmail.com>
2022-05-09 14:49:08 -0300

    Atualização Curso B7Web

--- ProjetoDevsbook/src/handlers/LoginHandler.php
+++ ProjetoDevsbook/src/handlers/LoginHandler.php
@@ -13,9 +13,9 @@ class LoginHandler {
             if(count($data) > 0) {
 
                 $loggedUser = new User();
-                $loggedUser->id = $data['id']; 
-                $loggedUser->email = $data['email'];
+                $loggedUser->id = $data['id'];
                 $loggedUser->name = $data['name'];
+                $loggedUser->avatar = $data['avatar'];
 
                 return $loggedUser;
             }
