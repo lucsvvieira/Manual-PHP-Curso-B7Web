409141c1bdd758efee2fe648a5b6ba23576aa0b6
Lucas Vieira <vieira07lucas@gmail.com>
2022-05-10 17:48:31 -0300

    Atualização Curso de PHP

--- ProjetoDevsbook/src/handlers/PostHandler.php
+++ ProjetoDevsbook/src/handlers/PostHandler.php
@@ -3,6 +3,8 @@
 namespace src\handlers;
 
 use \src\models\Post;
+use \src\models\User;
+use \src\models\UserRelation;
 
 class PostHandler
 {
@@ -20,5 +22,22 @@ class PostHandler
                 'body' => $body
             ])->execute();
         }
+    }
+
+    public static function getHomeFeed($idUser) {
+        // 1. Pegar lista de usuários que EU sigo
+        $userList = UserRelation::select()->where('user_from', $idUser)->get();
+        $users = [];
+        foreach($userList as $userItem) {
+            $users[] = $userItem['user_to'];
+        }
+        $users[] = $idUser;
+
+        print_r($users);
+
+        // 2. Pegar os posts dessa galera ordenado pela data.
+        // 3. Transformar o resultado em objetos dos models
+        // 4. Preencher as informações adicionais no post
+        // 5. Retornar o resultado
     }
 }
