e376aca34beb22f7767dbf2a68adce668ed10f29
Lucas Vieira <vieira07lucas@gmail.com>
2022-05-11 15:36:42 -0300

    Atualização do Curso de PHP da B7Web

--- ProjetoDevsbook/src/handlers/PostHandler.php
+++ ProjetoDevsbook/src/handlers/PostHandler.php
@@ -24,20 +24,45 @@ class PostHandler
         }
     }
 
-    public static function getHomeFeed($idUser) {
+    public static function getHomeFeed($idUser)
+    {
         // 1. Pegar lista de usuários que EU sigo
         $userList = UserRelation::select()->where('user_from', $idUser)->get();
         $users = [];
-        foreach($userList as $userItem) {
+        foreach ($userList as $userItem) {
             $users[] = $userItem['user_to'];
         }
         $users[] = $idUser;
 
-        print_r($users);
-
         // 2. Pegar os posts dessa galera ordenado pela data.
+        $postList = Post::select()
+            ->where('id_user', 'in', $users)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
         // 3. Transformar o resultado em objetos dos models
-        // 4. Preencher as informações adicionais no post
+        $posts = [];
+        foreach ($postList as $postItem) {
+            $newPost = new Post();
+            $newPost->id = $postItem['id'];
+            $newPost->type = $postItem['type'];
+            $newPost->created_at = $postItem['created_at'];
+            $newPost->body = $postItem['body'];
+
+            // 4. Preencher as informações adicionais no post
+            $newUser = User::select()->where('id', $postItem['id_user'])->one();
+            $newPost->user = new User();
+            $newPost->user->id = $newUser['id'];
+            $newPost->user->name = $newUser['name'];
+            $newPost->user->avatar = $newUser['avatar'];
+
+            // TO_do: 4.1 Preencher informações de LIKE
+            // TO_do: 4.2 Preencher informações de COMMENTS
+
+            $posts[] = $newPost;
+        }
+
         // 5. Retornar o resultado
+        return $posts;
     }
 }
