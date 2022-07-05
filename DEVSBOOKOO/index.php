c32cc82cae01de367f12ddb6eeee3ffbeb3dce98
Lucas Vieira <vieira07lucas@gmail.com>
2022-07-05 15:01:28 -0300

    Atualização Projeto Devsbook Orientado à Objetos

--- DEVSBOOKOO/index.php
+++ DEVSBOOKOO/index.php
@@ -4,3 +4,6 @@ require 'config.php';
 require 'models/auth.php';
 
 $auth = new Auth($pdo, $base);
+$userInfo = $auth->checkToken();
+
+echo 'Index';
--- DEVSBOOKOO/models/auth.php
+++ DEVSBOOKOO/models/auth.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'dao/UserDaoMySql.php';
 
 class Auth {
     private $pdo;
@@ -13,11 +14,37 @@ class Auth {
         if(!empty($_SESSION['token'])) {
             $token = $_SESSION['token'];
 
+            $userDao = new UserDaoMysql($this->pdo);
+            $user = $userDao->findByToken($token);
             
+            if($user) {
+                return $user;
+            }
         }
 
         header("Location: ".$this->base."/login.php");
         exit;
+    }
+
+    public function validateLogin($email, $password) {
+        $userDao = new UserDaoMysql($this->pdo);
+        $user = $userDao->findByEmail($email);
+
+        if($user) {
+
+            if(password_verify($password, $user->password)) {
+                $token = md5(time().rand(0,9999));
+
+                $_SESSION['token'] = $token;
+                $user->token = $token;
+                $userDao->update($user);
+
+                return true;
+            }
+
+        }
+
+        return false;
     }
 
 }
