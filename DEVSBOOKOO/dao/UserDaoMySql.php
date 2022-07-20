5f5f3fb0d30f1d5c3c52bd08f05bb9a50d85c713
Lucas Vieira <vieira07lucas@gmail.com>
2022-07-20 12:01:36 -0300

    Atualização Projeto Devsbook Orientado à Objetos

--- DEVSBOOKOO/dao/UserDaoMySql.php
+++ DEVSBOOKOO/dao/UserDaoMySql.php
@@ -27,10 +27,17 @@ class UserDaoMysql implements UserDAO {
 
             // Followers =  Quem segue o usuário
             $u->followers = $urDaoMysql->getFollowers($u->id);
+            foreach($u->followers as $key => $follower_id) {
+                $newUser = $this->findById($follower_id);
+                $u->followers[$key] = $newUser;
+            }
 
             // Following = Quem o usuário segue
             $u->following = $urDaoMysql->getFollowing($u->id);
-
+            foreach($u->following as $key => $follower_id) {
+                $newUser = $this->findById($follower_id);
+                $u->following[$key] = $newUser;
+            }
             // Fotos
             $u->photos = [];
         }
