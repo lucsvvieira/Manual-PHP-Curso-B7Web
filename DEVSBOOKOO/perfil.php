7c17e112848418e77182a6231ab9c4bc764bdbf9
Lucas Vieira <vieira07lucas@gmail.com>
2022-07-22 12:12:50 -0300

    Atualização Projeto Devsboook Orientado à Objetos

--- DEVSBOOKOO/perfil.php
+++ DEVSBOOKOO/perfil.php
@@ -7,10 +7,16 @@ require 'dao/PostDaoMysql.php';
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
 $activeMenu = 'profile';
+$user = [];
+$feed = [];
 
 $id = filter_input(INPUT_GET, 'id');
 if (!$id) {
     $id = $userInfo->id;
+}
+
+if ($id != $userInfo->id) {
+    $activeMenu = '';
 }
 
 $postDao = new PostDaoMysql($pdo);
@@ -27,6 +33,7 @@ $dateTo = new DateTime('today');
 $user->ageYears = $dateFrom->diff($dateTo)->y;
 
 // Pegar o feed do usuário
+$feed = $postDao->getUserFeed($id);
 
 // Verificar se eu sigo este usuário
 
@@ -114,134 +121,63 @@ require 'partials/menu.php';
                     <?php if (count($user->following) > 0) : ?>
                         <?php foreach ($user->following as $item) : ?>
                             <div class="friend-icon">
-                                <a href="<?=$base;?>/perfil.php?id=<?=$item->id;?>">
+                                <a href="<?= $base; ?>/perfil.php?id=<?= $item->id; ?>">
                                     <div class="friend-icon-avatar">
-                                        <img src="<?=$base;?>/media/avatars/<?=$item->avatar;?>" />
+                                        <img src="<?= $base; ?>/media/avatars/<?= $item->avatar; ?>" />
                                     </div>
                                     <div class="friend-icon-name">
-                                        <?=$item->name;?>
+                                        <?= $item->name; ?>
                                     </div>
                                 </a>
                             </div>
                         <?php endforeach; ?>
                     <?php endif; ?>
-                    
-        </div>
-        <div class="column pl-5">
-
-            <div class="box">
-                <div class="box-header m-10">
-                    <div class="box-header-text">
-                        Fotos
-                        <span>(<?=count($user->photos);?>)</span>
-                    </div>
-                    <div class="box-header-buttons">
-                        <a href="<?=$base;?>/fotos.php?id=<?=$user->id;?>">ver todos</a>
-                    </div>
-                </div>
-                <div class="box-body row m-20">
-
-                    <div class="user-photo-item">
-                        <a href="#modal-1" rel="modal:open">
-                            <img src="media/uploads/1.jpg" />
-                        </a>
-                        <div id="modal-1" style="display:none">
-                            <img src="media/uploads/1.jpg" />
-                        </div>
-                    </div>
-
-                    <div class="user-photo-item">
-                        <a href="#modal-2" rel="modal:open">
-                            <img src="media/uploads/1.jpg" />
-                        </a>
-                        <div id="modal-2" style="display:none">
-                            <img src="media/uploads/1.jpg" />
-                        </div>
-                    </div>
-
-                    <div class="user-photo-item">
-                        <a href="#modal-3" rel="modal:open">
-                            <img src="media/uploads/1.jpg" />
-                        </a>
-                        <div id="modal-3" style="display:none">
-                            <img src="media/uploads/1.jpg" />
-                        </div>
-                    </div>
-
-                    <div class="user-photo-item">
-                        <a href="#modal-4" rel="modal:open">
-                            <img src="media/uploads/1.jpg" />
-                        </a>
-                        <div id="modal-4" style="display:none">
-                            <img src="media/uploads/1.jpg" />
-                        </div>
-                    </div>
 
                 </div>
+                <div class="column pl-5">
+
+                    <div class="box">
+                        <div class="box-header m-10">
+                            <div class="box-header-text">
+                                Fotos
+                                <span>(<?= count($user->photos); ?>)</span>
+                            </div>
+                            <div class="box-header-buttons">
+                                <a href="<?= $base; ?>/fotos.php?id=<?= $user->id; ?>">ver todos</a>
+                            </div>
+                        </div>
+                        <div class="box-body row m-20">
+
+                            <?php if (count($user->photos) > 0) : ?>
+                                <?php foreach ($user->photos as $item) : ?>
+                                    <div class="user-photo-item">
+                                        <a href="#modal-1" rel="modal:open">
+                                            <img src="media/uploads/1.jpg" />
+                                        </a>
+                                        <div id="modal-1" style="display:none">
+                                            <img src="media/uploads/1.jpg" />
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+
+                        </div>
+                    </div>
+
+                    <?php if ($id == $userInfo->id) : ?>
+                        <?php require 'partials/feed-editor.php'; ?>
+                    <?php endif; ?>
+
+                    <?php if (count($feed) > 0) : ?>
+                        <?php foreach ($feed as $item) : ?>
+                            <?php require 'partials/feed-item.php'; ?>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        Não há postagens deste usuário.
+                    <?php endif; ?>
+                </div>
+
             </div>
-
-            <div class="box feed-item">
-                <div class="box-body">
-                    <div class="feed-item-head row mt-20 m-width-20">
-                        <div class="feed-item-head-photo">
-                            <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                        </div>
-                        <div class="feed-item-head-info">
-                            <a href=""><span class="fidi-name">Bonieky Lacerda</span></a>
-                            <span class="fidi-action">fez um post</span>
-                            <br />
-                            <span class="fidi-date">07/03/2020</span>
-                        </div>
-                        <div class="feed-item-head-btn">
-                            <img src="assets/images/more.png" />
-                        </div>
-                    </div>
-                    <div class="feed-item-body mt-10 m-width-20">
-                        Pessoal, tudo bem! Busco parceiros para empreender comigo em meu software.<br /><br />
-                        Acabei de aprová-lo na Appstore. É um sistema de atendimento via WhatsApp multi-atendentes para auxiliar empresas.<br /><br />
-                        Este sistema permite que vários funcionários/colaboradores da empresa atendam um mesmo número de WhatsApp, mesmo que estejam trabalhando remotamente, sendo que cada um acessa com um login e senha particular....
-                    </div>
-                    <div class="feed-item-buttons row mt-20 m-width-20">
-                        <div class="like-btn on">56</div>
-                        <div class="msg-btn">3</div>
-                    </div>
-                    <div class="feed-item-comments">
-
-                        <div class="fic-item row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <div class="fic-item-info">
-                                <a href="">Bonieky Lacerda</a>
-                                Comentando no meu próprio post
-                            </div>
-                        </div>
-
-                        <div class="fic-item row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <div class="fic-item-info">
-                                <a href="">Bonieky Lacerda</a>
-                                Muito legal, parabéns!
-                            </div>
-                        </div>
-
-                        <div class="fic-answer row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <input type="text" class="fic-item-field" placeholder="Escreva um comentário" />
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-
-    </div>
 
 </section>
 <?php
