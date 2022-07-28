0620517ba98380c87fd4e0f4f3e7c180a8f69929
Lucas Vieira <vieira07lucas@gmail.com>
2022-07-28 11:43:39 -0300

    Atualização Projeto Devsbook Orientado à Objetos

--- DEVSBOOKOO/configuracoes_action.php
+++ DEVSBOOKOO/configuracoes_action.php
@@ -60,11 +60,8 @@ if($name && $email) {
         }
     }
 
-    echo '<pre>';
-    print_r($_FILES);
-
     // Validações do Avatar
-    if(isset($_FILES[$avatar]) && !empty($_FILES['avatar']['tmp_name'])) {
+    if(isset($_FILES['avatar']) && !empty($_FILES['avatar']['tmp_name'])) {
         $newAvatar = $_FILES['avatar'];
 
         if(in_array($newAvatar['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
@@ -81,6 +78,84 @@ if($name && $email) {
                 $newHeight = $avatarHeight;
                 $newWidth = $newHeight * $ratio;
             }
+
+            $x = $avatarWidth - $newWidth;
+            $y = $avatarHeight - $newHeight;
+            $x = $x<0 ? $x/2 : $x;
+            $y = $y<0 ? $y/2 : $y;
+
+            $finalImage = imagecreatetruecolor($avatarWidth, $avatarHeight);
+            switch($newAvatar['type']) {
+                case 'image/jpeg':
+                case 'image/jpg':
+                    $image = imagecreatefromjpeg($newAvatar['tmp_name']);
+                break;
+                case 'image/png':
+                    $image = imagecreatefrompng($newAvatar['tmp_name']);
+                break;
+            }
+
+            imagecopyresampled(
+                $finalImage, $image,
+                $x, $y, 0, 0,
+                $newWidth, $newHeight, $widthOrig, $heightOrig
+            );
+
+            $avatarName = md5(time().rand(0, 9999)).'.jpg';
+
+            imagejpeg($finalImage, './media/avatars/'.$avatarName, 100);
+
+            $userInfo->avatar = $avatarName;
+
+        }
+    }
+
+    // Validações da Capa
+    if(isset($_FILES['cover']) && !empty($_FILES['cover']['tmp_name'])) {
+        $newCover = $_FILES['cover'];
+
+        if(in_array($newCover['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+            $coverWidth = 850;
+            $coverHeight = 310;
+
+            list($widthOrig, $heightOrig) = getimagesize($newCover['tmp_name']);
+            $ratio = $widthOrig / $heightOrig;
+
+            $newWidth = $coverWidth;
+            $newHeight = $newWidth / $ratio;
+
+            if($newHeight < $coverHeight) {
+                $newHeight = $coverHeight;
+                $newWidth = $newHeight * $ratio;
+            }
+
+            $x = $coverWidth - $newWidth;
+            $y = $coverHeight - $newHeight;
+            $x = $x<0 ? $x/2 : $x;
+            $y = $y<0 ? $y/2 : $y;
+
+            $finalImage = imagecreatetruecolor($coverWidth, $coverHeight);
+            switch($newCover['type']) {
+                case 'image/jpeg':
+                case 'image/jpg':
+                    $image = imagecreatefromjpeg($newCover['tmp_name']);
+                break;
+                case 'image/png':
+                    $image = imagecreatefrompng($newCover['tmp_name']);
+                break;
+            }
+
+            imagecopyresampled(
+                $finalImage, $image,
+                $x, $y, 0, 0,
+                $newWidth, $newHeight, $widthOrig, $heightOrig
+            );
+
+            $coverName = md5(time().rand(0, 9999)).'.jpg';
+
+            imagejpeg($finalImage, './media/covers/'.$coverName, 100);
+
+            $userInfo->cover = $coverName;
 
         }
     }
