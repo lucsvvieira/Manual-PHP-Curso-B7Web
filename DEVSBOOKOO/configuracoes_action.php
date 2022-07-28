a007938f58c7fb6be1dfbcf3edd83999b01436f7
Lucas Vieira <vieira07lucas@gmail.com>
2022-07-28 11:21:52 -0300

    Atualização Projeto Devsbook Orientado à Objetos

--- DEVSBOOKOO/configuracoes_action.php
+++ DEVSBOOKOO/configuracoes_action.php
@@ -60,6 +60,31 @@ if($name && $email) {
         }
     }
 
+    echo '<pre>';
+    print_r($_FILES);
+
+    // Validações do Avatar
+    if(isset($_FILES[$avatar]) && !empty($_FILES['avatar']['tmp_name'])) {
+        $newAvatar = $_FILES['avatar'];
+
+        if(in_array($newAvatar['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+            $avatarWidth = 200;
+            $avatarHeight = 200;
+
+            list($widthOrig, $heightOrig) = getimagesize($newAvatar['tmp_name']);
+            $ratio = $widthOrig / $heightOrig;
+
+            $newWidth = $avatarWidth;
+            $newHeight = $newWidth / $ratio;
+
+            if($newHeight < $avatarHeight) {
+                $newHeight = $avatarHeight;
+                $newWidth = $newHeight * $ratio;
+            }
+
+        }
+    }
+
     $userDao->update($userInfo);
 }
 
