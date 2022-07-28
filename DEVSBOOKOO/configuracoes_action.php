e2833dd9ef3eb5ff262b1af81680935fed73db45
Lucas Vieira <vieira07lucas@gmail.com>
2022-07-28 10:42:58 -0300

    Atualização Projeto Devsbook Orientado à Objetos

---
+++ DEVSBOOKOO/configuracoes_action.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'config.php';
+require_once 'models/Auth.php';
+require_once 'dao/UserDaoMysql.php';
+
+$auth = new Auth($pdo, $base);
+$userInfo = $auth->checkToken();
+
+$userDao = new UserDaoMysql($pdo);
+
+$name = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$birthdate = filter_input(INPUT_POST, 'birthdate');
+$city = filter_input(INPUT_POST, 'city');
+$work = filter_input(INPUT_POST, 'work');
+$password = filter_input(INPUT_POST, 'password');
+$password_confirmation = filter_input(INPUT_POST, 'password_confirmation');
+
+if($name && $email) {
+    $userInfo->name = $name;
+    $userInfo->city = $city;
+    $userInfo->work = $work;
+
+    if($userInfo->email != $email) {
+        if($userDao->findByEmail($email) === false) {
+            $userInfo->email = $email;
+        } else {
+            $_SESSION['flash'] = 'E-mail já cadastrado!';
+            header("Location: ".$base."/configuracoes.php");
+            exit;
+        }
+    }
+
+    //$userDao->update($userInfo);
+}
+
+header("Location: ".$base."/configuracoes.php");
+exit;
