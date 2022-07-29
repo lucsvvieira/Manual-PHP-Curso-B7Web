825f6abfa93e62b9e47b68522e66d1776862f7a6
Lucas Vieira <vieira07lucas@gmail.com>
2022-07-29 11:29:10 -0300

    Atualização Projeto Devsbook Orientado à Objetos

--- DEVSBOOKOO/partials/feed-item.php
+++ DEVSBOOKOO/partials/feed-item.php
@@ -37,7 +37,17 @@ switch($item->type) {
         </div>
         <div class="feed-item-comments">
             <div class="feed-item-comments-area">
-                
+                <?php foreach($item->comments as $comment): ?>
+                    <div class="fic-item row m-height-10 m-width-20">
+                        <div class="fic-item-photo">
+                            <a href="<?=$base;?>/perfil.php?id=<?=$comment->id_user;?>"><img src="<?=$base;?>/media/avatars/<?=$comment->user->avatar;?>" ></a>
+                        </div>
+                        <div class="fic-item-info">
+                            <a href="<?=$base;?>/perfil.php?id=<?=$comment->id_user;?>"><?=$comment->user->name;?></a>
+                            <?=$comment->body;?>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
            
 
